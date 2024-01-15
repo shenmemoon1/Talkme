4edbcea9ceff01b29a1ebf4560bcfe21e6128ac5
billlims <davidlinxf@gmail.com>
2024-01-15 22:43:02 +1100

    defind database model and controller loginc

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,4 +1,26 @@
 const User = require("../model/userModel");
-module.exports.register = (req, res, next) => {
-  console.log(req.body);
+const bcrypt = require("bcrypt");
+module.exports.register = async (req, res, next) => {
+  try {
+    const { username, password, email } = req.body;
+    const usernameCheck = await User.findOne({ username });
+    if (usernameCheck) {
+      return res.json({ msg: "Username already use", status: false });
+    }
+    const emailCheck = await User.findOne({ email });
+    if (emailCheck) {
+      res.json({ msg: "Email already use", status: false });
+    }
+    const hashPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({
+      email,
+      username,
+      password: hashPassword,
+    });
+
+    delete user.password;
+    return res.json({ status: true, user });
+  } catch (error) {
+    next(error);
+  }
 };
