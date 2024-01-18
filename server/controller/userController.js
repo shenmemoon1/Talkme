9115a1f4768cc46421f0fd4755e2d18cab83e51c
billlims <davidlinxf@gmail.com>
2024-01-18 22:18:03 +1100

    fetch user list

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -64,3 +64,18 @@ module.exports.setAvatar = async (req, res, next) => {
     next(error);
   }
 };
+
+// get users
+module.exports.getUsers = async (req, res, next) => {
+  try {
+    const users = await User.find({ _id: { $ne: req.params.id } }).select([
+      "email",
+      "username",
+      "avatarImage",
+      "_id",
+    ]);
+    return res.json(users);
+  } catch (error) {
+    next(error);
+  }
+};
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,10 +1,15 @@
 const express = require("express");
-const { register, login, setAvatar } = require("../controller/userController");
+const {
+  register,
+  login,
+  setAvatar,
+  getUsers,
+} = require("../controller/userController");
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
 router.post("/setavatar/:id", setAvatar);
-router.get("/allusers");
+router.get("/allusers/:id", getUsers);
 
 module.exports = router;
